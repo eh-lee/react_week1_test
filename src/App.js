93a62135849b39565f535a354595c6c6046f99a8
eh-lee <fornewstack@daum.net>
2023-03-02 10:24:17 +0900

    test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [todo, setTodo] = useState([
+    {
+      id: 1,
+      title: "title",
+    }
+  ])
+
+  const [title, setTitle] = useState('');
+
+  const titleHandler = (title) => {
+    setTitle(title.target.value)
+  }
+
+  const addBtnHandler = () => {
+    const newTodo = {
+      id: todo.length+1,
+      title,
+    }
+    setTodo([...todo, newTodo]);
+    setTitle('')
 }
 
-export default App;
+  return (
+    <div>
+      <div className="todo_list">
+        <input placeholder="자바스크립트를 배워 봅시다" type='text' value ={title} onChange={titleHandler}/>
+        <button onClick={addBtnHandler}>추가하기</button><br/>
+        <h1>Todo List</h1>
+      </div>
+
+      <div>
+        {todo.map(todo => {
+        return(
+          <div key={todo.id}>
+            <h5 className="todo_cont">{todo.title}</h5>
+          </div>
+        )
+        })}
+      </div>
+
+    </div>
+  )
+}
+
+export default App
